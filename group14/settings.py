036ecda04f750ab52ba023ec5ed8ca7d5fa7720b
Alimam Haruna Seidu <harunaseidujnr@gmail.com>
2022-05-24 09:21:02 +0000

    added whitenoise middleware

--- group14/settings.py
+++ group14/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'group14.urls'
@@ -125,6 +126,8 @@ STATICFILES_DIRS=[
 STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 MEDIA_URL= '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
